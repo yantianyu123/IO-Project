b24f16538b11f1ce3dfd4ce801f95efa54968def
yantianyu <2466707075@qq.com>
2019-07-06 14:42:02 +0800

    第3次提交IO-Projects:添加了一个junit单元测试方法

--- Demo1/IO-Projects/src/Demo1_test.java
+++ Demo1/IO-Projects/src/Demo1_test.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 /**
  * @author 闫天宇
  * @date 2019/7/6-14:33
@@ -6,6 +8,10 @@ public class Demo1_test {
 
     public static void main(String[] args) {
         System.out.println("测试");
+    }
+    @Test
+    public void test1(){
+        System.out.println("使用junit单元测试");
     }
 }
 
